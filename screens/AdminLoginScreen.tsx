b53229b1732bede6406641fdb0cc9e3b247ba10a
asimiox <onlineaxim@gmail.com>
2026-02-07 15:32:32 +0500

    Add files via upload

---
+++ screens/AdminLoginScreen.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+// Placeholder component to prevent build errors on empty files
+export const AdminLoginScreen = () => {
+  return <div>Admin Login Screen Placeholder</div>;
+};
